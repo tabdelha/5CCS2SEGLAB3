b5ad86d0cc9087528eddd5e447d2d78e7e253b3d
tabdelha <tala.abdelhamid@hotmail.com>
2017-10-27 12:05:21 +0100

    Add check whether user input is prime number

--- tools03/src/main/java/PrimeNumber.java
+++ tools03/src/main/java/PrimeNumber.java
@@ -4,12 +4,15 @@ import java.util.*;
 
 
 
+
+
 public class PrimeNumber
 
 {
     
+    
     public static void main(String [] args){
-        
+       
         System.out.println("Prime Number Checker");
         
         Scanner sc = new Scanner(System.in);
@@ -17,22 +20,73 @@ public class PrimeNumber
         try
         
         {
+            
             System.out.println("Please input an integer");
             
+        
             int usrInput = 0;
             
             usrInput = sc.nextInt();
-        
-            System.out.println("Integer");
+            
+            int j = 2;
+            
+            int result = 0;
+            
+            while (j <= (usrInput / 2))
+                
+            {
+                
+                if ( (usrInput %j) == 0)
+                    
+                {
+                    
+                    result = 1;
+                    
+                }
+                
+                j++;
+                
+            }
+            
+            
+            
+            if (result==1)
+                
+            {
+                System.out.println("This integer is not a prime number");
+                
+            }
+            
+            else
+                
+            {
+                
+                System.out.println("This integer is a prime number");
+                
+            }
+            
+           
+            
+            
+            
+            
         }
+        
+        
         
         catch (InputMismatchException exception)
         
         {
+            
             System.out.println("Not an integer");
             
         }
         
+        
+        
     }
     
+    
+    
 }
+
